b28b31542fed41268e145d91857b1e7b1fed705f
Andrii Kondratiev <h0st@ukr.net>
2026-02-19 10:02:50 +0200

    Added check for running only one application

--- Alpha/Alpha/AppDelegate.swift
+++ Alpha/Alpha/AppDelegate.swift
@@ -7,10 +7,32 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
         LocalizationManager.configureIfNeeded()
+
+        if isAnotherInstanceRunning() {
+            showAlreadyRunningAlert()
+            NSApp.terminate(nil)
+            return
+        }
+
         statusBarController = StatusBarController()
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return false
+    }
+
+    private func isAnotherInstanceRunning() -> Bool {
+        guard let bundleId = Bundle.main.bundleIdentifier else { return false }
+        let apps = NSRunningApplication.runningApplications(withBundleIdentifier: bundleId)
+        return apps.count > 1
+    }
+
+    private func showAlreadyRunningAlert() {
+        NSApp.activate(ignoringOtherApps: true)
+        let alert = NSAlert()
+        alert.messageText = LocalizationManager.text("already_running_title")
+        alert.informativeText = LocalizationManager.text("already_running_body")
+        alert.addButton(withTitle: LocalizationManager.text("ok_button"))
+        alert.runModal()
     }
 }
